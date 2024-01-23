4c1e2632dbb71da07a9b6b1dabd36ce257b74981
Andrija Stanisic <andrija.d.stanisic@gmail.com>
2024-01-23 14:20:28 +0100

    bug fix

--- mpi_parallel/main.c
+++ mpi_parallel/main.c
@@ -6,58 +6,47 @@
 
 #define d 256
 
-void search(char *patterns[], int num_patterns, char *txt, int mod, int rank, int num_procs)
+int search(char *pat, char *txt, int q)
 {
-    for (int k = 0; k < num_patterns; k++)
+    int M = strlen(pat);
+    int N = strlen(txt);
+    int i, j;
+    int p = 0;
+    int t = 0;
+    int h = 1;
+    int count = 0;
+
+    for (i = 0; i < M - 1; i++)
+        h = (h * d) % q;
+
+    for (i = 0; i < M; i++)
     {
-        char *pat = patterns[k];
-        int M = strlen(pat);
-        int N = strlen(txt);
-        int i, j;
-        int p = 0, t = 0, count = 0, h = 1;
+        p = (d * p + pat[i]) % q;
+        t = (d * t + txt[i]) % q;
+    }
 
-        for (i = 0; i < M - 1; i++)
-            h = (h * d) % mod;
-
-        for (i = 0; i < M; i++)
+    for (i = 0; i <= N - M; i++)
+    {
+        if (p == t)
         {
-            p = (d * p + pat[i]) % mod;
-            t = (d * t + txt[i]) % mod;
-        }
-
-        int local_count = 0;
-
-        for (i = rank; i <= N - M; i += num_procs)
-        {
-            if (p == t)
+            for (j = 0; j < M; j++)
             {
-                for (j = 0; j < M; j++)
-                {
-                    if (txt[i + j] != pat[j])
-                        break;
-                }
-
-                if (j == M)
-                {
-                    local_count++;
-                }
+                if (txt[i + j] != pat[j])
+                    break;
             }
 
-            if (i < N - M)
-            {
-                t = (d * (t - txt[i] * h) + txt[i + M]) % mod;
-                if (t < 0)
-                    t = (t + mod);
-            }
+            if (j == M)
+                count++;
         }
 
-        MPI_Allreduce(&local_count, &count, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-
-        if (rank == 0)
+        if (i < N - M)
         {
-            printf("Total occurrences of '%s': %d\n", pat, count);
+            t = (d * (t - txt[i] * h) + txt[i + M]) % q;
+            if (t < 0)
+                t = (t + q);
         }
     }
+    return count;
 }
 
 char *read_file(char *filepath)
@@ -92,44 +81,64 @@ char *read_file(char *filepath)
     return buffer;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+    int size, rank, text_size;
+    char *text = NULL;
+    int local_counts[3] = {0, 0, 0};
+    int global_counts[3] = {0, 0, 0};
+    double start_time, end_time;
+
     MPI_Init(&argc, &argv);
-
-    int rank, num_procs;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    printf("#################################\n");
-    printf("Initiating the parallel algorithm...\n");
+    if (rank == 0)
+    {
+        start_time = MPI_Wtime();
+        char *filepath = "../scripts/controller_input.txt";
+        text = read_file(filepath);
+        if (text == NULL)
+        {
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        text_size = strlen(text);
+    }
 
-    char *filepath = "../scripts/controller_input.txt";
-    char *buffer = read_file(filepath);
+    MPI_Bcast(&text_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    int segment_size = text_size / size;
+    char *segment = malloc(segment_size + 1);
+
+    MPI_Scatter(text, segment_size, MPI_CHAR, segment, segment_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+    segment[segment_size] = '\0';
+
     char *patterns[] = {"R1L1X", "R1XO", "OYO"};
-    int mod = 101;
+    int num_patterns = 3;
 
-    clock_t start_time, end_time;
-    double elapsed_time;
+    for (int i = 0; i < num_patterns; i++)
+    {
+        local_counts[i] = search(patterns[i], segment, 101);
+    }
+
+    MPI_Reduce(local_counts, global_counts, num_patterns, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (rank == 0)
     {
-        start_time = clock();
+        end_time = MPI_Wtime();
+        for (int i = 0; i < num_patterns; i++)
+        {
+            printf("Total occurrences of '%s': %d\n", patterns[i], global_counts[i]);
+        }
+        printf("Total execution time: %f seconds\n", end_time - start_time);
     }
 
-    search(patterns, 3, buffer, mod, rank, num_procs);
-
+    free(segment);
     if (rank == 0)
     {
-        end_time = clock();
-        elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-        printf("Execution time: %f seconds\n", elapsed_time);
-        free(buffer);
+        free(text);
     }
-
-    printf("Algorithm completed.\n");
-    printf("#################################\n");
 
     MPI_Finalize();
-
     return 0;
 }
